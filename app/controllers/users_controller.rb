6d50d63042592cab74ab41f81028c3186847b5d4
Noah Solnick <soltrain@berkeley.edu>
2013-10-09 20:08:31 +0800

    halfway through 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  #catch attempts to access edit/update for anyone not signed in
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  #catch attempts to access edit/update for anyone but current user
+  before_action :correct_user, only: [:edit, :update] 
 
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
@@ -19,12 +27,49 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    #previously had @user = User.find(params[:id])
+    #now covered by the correct user before action
+  end
+
+  def update
+    #see note for edit
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+
   private
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  #before filters
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+
+    #following is the same as:
+    #unless signed_in?
+    #flash[:notice] = "Please sign in."
+    #redirect_to signin_url
+    #here it is: 
+    #redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
 end
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,17 @@
+#old code to just create one user
+#FactoryGirl.define do
+#	factory :user do
+#		name "Michael Hartl"
+#		email "michael@example.com"
+#		password "foobar"
+#		password_confirmation "foobar"
+#	end
+#end
+
 FactoryGirl.define do
 	factory :user do
-		name "Michael Hartl"
-		email "michael@example.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
 	end
